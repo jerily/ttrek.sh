42800ec4bcb9d778b499019210501a37a65e8d2a
Neofytos Dimitriou <neo@jerily.cy>
2024-04-03 05:27:57 -0400

    added basic functionality like showing packages, their versions, and downloading the spec and install script

--- app.tcl
+++ app.tcl
@@ -10,28 +10,26 @@ set init_script {
 
     ::thtml::init [dict create \
         cache 0 \
-        rootdir [::twebserver::get_rootdir]]
+        rootdir [file join [file dirname [info script]] www]]
 
-    # create a router
     set router [::twebserver::create_router]
 
-    # add a route that will be called if the request method is GET and the path is "/"
     ::twebserver::add_route -strict $router GET / get_index_page_handler
-
-    # add a route that will be called if the request method is GET and the path is "/blog"
-    ::twebserver::add_route -strict $router GET /blog get_blog_handler
-
-    # add a route that will be called if the request method is GET and the path is "/logo"
+    ::twebserver::add_route $router -strict GET /package/:package_name get_package_page_handler
+    ::twebserver::add_route $router -strict GET /registry/:package_name/:package_version get_package_spec_handler
+    ::twebserver::add_route $router -strict GET /registry/:package_name/:package_version/install.sh get_package_script_handler
     ::twebserver::add_route -strict $router GET /logo get_logo_handler
-
-    # add a catchall route that will be called if no other route matches a GET request
     ::twebserver::add_route $router GET "*" get_catchall_handler
 
     # make sure that the router will be called when the server receives a connection
     interp alias {} process_conn {} $router
 
     proc get_index_page_handler {ctx req} {
-        set data [dict merge $req {title "My Index Page" content "Hello World!"}]
+        set package_names [list]
+        foreach path [glob -nocomplain -type d [file join [::twebserver::get_rootdir] registry/*]] {
+            lappend package_names [file tail $path]
+        }
+        set data [dict merge $req [list title "Packages" package_names [lsort $package_names]]]
         set html [::thtml::renderfile index.thtml $data]
         set res [::twebserver::build_response 200 text/html $html]
         return $res
@@ -39,20 +37,46 @@ set init_script {
 
     proc get_logo_handler {ctx req} {
         set server_handle [dict get $ctx server]
-        set dir [::twebserver::get_rootdir $server_handle]
+        set dir [::thtml::get_rootdir]
         set filepath [file join $dir plume.png]
         set res [::twebserver::build_response -return_file 200 image/png $filepath]
         return $res
     }
 
-    proc get_blog_handler {ctx req} {
-        set posts {
-            {title "My First Post" content "This is my first post"}
-            {title "My Second Post" content "This is my second post"}
-            {title "My Third Post" content "This is my third post"}
+    proc get_package_spec_handler {ctx req} {
+        set package_name [::twebserver::get_path_param $req package_name]
+        set package_version [::twebserver::get_path_param $req package_version]
+        set dir [::twebserver::get_rootdir]
+        set filepath [file join $dir registry $package_name $package_version ttrek.json]
+        if {![file exists $filepath]} {
+            set res [::twebserver::build_response 404 text/plain "not found"]
+            return $res
         }
-        set data [dict merge $req [list posts $posts]]
-        set html [::thtml::renderfile blog.thtml $data]
+        set res [::twebserver::build_response -return_file 200 application/json $filepath]
+        return $res
+    }
+
+    proc get_package_script_handler {ctx req} {
+        set package_name [::twebserver::get_path_param $req package_name]
+        set package_version [::twebserver::get_path_param $req package_version]
+        set dir [::twebserver::get_rootdir]
+        set filepath [file join $dir registry $package_name $package_version install.sh]
+        if {![file exists $filepath]} {
+            set res [::twebserver::build_response 404 text/plain "not found"]
+            return $res
+        }
+        set res [::twebserver::build_response -return_file 200 text/plain $filepath]
+        return $res
+    }
+
+    proc get_package_page_handler {ctx req} {
+        set package_name [::twebserver::get_path_param $req package_name]
+        set versions [list]
+        foreach path [glob -nocomplain -type d [file join [::twebserver::get_rootdir] registry $package_name/*]] {
+            lappend versions [file tail $path]
+        }
+        set data [dict merge $req [list package_name $package_name versions [lsort -decreasing $versions]]]
+        set html [::thtml::renderfile package.thtml $data]
         set res [::twebserver::build_response 200 text/html $html]
         return $res
     }
@@ -67,7 +91,7 @@ set init_script {
 # use threads and gzip compression
 set config_dict [dict create \
     num_threads 10 \
-    rootdir [file join [file dirname [info script]] www] \
+    rootdir [file dirname [info script]] \
     gzip on \
     gzip_types [list text/plain application/json] \
     gzip_min_length 20]
---
+++ registry/tjson/1.0.11/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eo pipefail # exit on error
+
+export SCRIPT_DIR=$1
+INSTALL_DIR=$SCRIPT_DIR/local
+echo "Installing to $INSTALL_DIR"
+
+BUILD_DIR=$SCRIPT_DIR/build
+mkdir -p $BUILD_DIR
+
+BUILD_LOG_DIR=$BUILD_DIR/logs
+mkdir -p $BUILD_LOG_DIR
+export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$INSTALL_DIR/lib64
+export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig
+
+# thtml
+if true; then
+  VERSION=1.0.11
+  curl -L -o tjson-$VERSION.tar.gz --output-dir $BUILD_DIR https://github.com/jerily/tjson/archive/refs/tags/v1.0.11.tar.gz
+  tar -xzvf $BUILD_DIR/tjson-$VERSION.tar.gz -C $BUILD_DIR
+  cd $BUILD_DIR/tjson-$VERSION
+  mkdir build
+  cd build
+  cmake .. \
+    -DTCL_LIBRARY_DIR=$INSTALL_DIR/lib \
+    -DTCL_INCLUDE_DIR=$INSTALL_DIR/include \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DCMAKE_PREFIX_PATH=$INSTALL_DIR/ > $BUILD_LOG_DIR/tjson-configure.log 2>&1
+  make install > $BUILD_LOG_DIR/tjson-install.log 2>&1
+fi
