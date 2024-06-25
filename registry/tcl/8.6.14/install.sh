d7992a6976807d883770e8cf7a05f8a6c1b93f8c
Neofytos Dimitriou <neo@jerily.cy>
2024-06-25 01:54:55 -0400

    renamed "curl" command to "download" + other minor changes to spec/install files

---
+++ registry/tcl/8.6.14/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail # exit on error
+
+export SCRIPT_DIR=$1
+INSTALL_DIR=$SCRIPT_DIR/local
+echo "Installing to $INSTALL_DIR"
+
+BUILD_DIR=$SCRIPT_DIR/build
+mkdir -p $BUILD_DIR
+
+BUILD_LOG_DIR=$BUILD_DIR/logs
+mkdir -p $BUILD_LOG_DIR
+export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$INSTALL_DIR/lib64
+export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig
+
+# tcl
+if true; then
+  curl -L -O --output-dir $BUILD_DIR http://prdownloads.sourceforge.net/tcl/tcl8.6.14-src.tar.gz
+  tar -xvf $BUILD_DIR/tcl8.6.14-src.tar.gz -C $BUILD_DIR
+  cd $BUILD_DIR/tcl8.6.14/unix
+  ./configure  --prefix=$INSTALL_DIR > $BUILD_LOG_DIR/tcl-configure.log 2>&1
+  make install > $BUILD_LOG_DIR/tcl-install.log 2>&1
+fi
